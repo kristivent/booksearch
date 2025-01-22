56334bef9fb9a261a579dd1c61d7c0be03c6737b
kristivent <ventk84@gmail.com>
2025-01-22 13:26:06 -0500

    updated code in resolvers.ts and typeDefs.ts

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,1 +1,44 @@
-//define the query and mutation functionality to work with mongoose models
+// define the query and mutation functionality to work with mongoose models
+import { getSingleUser, createUser, login, saveBook, deleteBook } from '../controllers/user-controller.js';
+import { AuthenticationError } from 'apollo-server-express';
+
+const resolvers = {
+  Query: {
+    // Get a single user by id or username
+    me: async (_parent: unknown, _args: unknown, context: any) => {
+      if (context.user) {
+        return getSingleUser(context.req, context.res);
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+  Mutation: {
+    // Create a user, sign a token, and send back
+    addUser: async (_parent: unknown, _args: unknown, context: any) => {
+      return createUser(context.req, context.res);
+    },
+    // Login a user, sign a token, and send back
+    login: async (_parent: unknown, args: any, context: any) => {
+      context.req.body = args;
+      return login(context.req, context.res);
+    },
+    // Save a book to a user's `savedBooks` field by adding it to the set
+    saveBook: async (_parent: unknown, args: any, context: any) => {
+      if (context.user) {
+        context.req.body = args;
+        return saveBook(context.req, context.res);
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    // Remove a book from `savedBooks`
+    deleteBook: async (_parent: unknown, args: any, context: any) => {
+      if (context.user) {
+        context.req.body = args;
+        return deleteBook(context.req, context.res);
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+export default resolvers;
